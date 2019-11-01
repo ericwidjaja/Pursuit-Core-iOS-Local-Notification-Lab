4368ff98c181ef455dffad5e21731aa1d0905f7f
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-01 15:58:57 -0400

    Create the project

---
+++ Local-Notification-Lab/Local-Notification-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Local-Notification-Lab
+//
+//  Created by Eric Widjaja on 11/1/19.
+//  Copyright © 2019 Eric W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
